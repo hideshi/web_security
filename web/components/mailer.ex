55e4ce1bacbb93275fb3cb2388be1ea9ed5f2b36
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2015-11-23 06:50:18 +0800

    Modified mailer setting

--- web/components/mailer.ex
+++ web/components/mailer.ex
@@ -1,11 +1,9 @@
 defmodule WebSecurity.Mailer do
   use Mailgun.Client,
     domain: Application.get_env(:web_security, :mailgun_domain),
-    key: Application.get_env(:web_security, :mailgun_key),
-    mode: :test,
-    test_file_path: "/tmp/mailgun.json"
+    key: Application.get_env(:web_security, :mailgun_key)
 
-  @from "hideshi.ogoshi@gmail.com"
+  @from "postmaster@sandbox37ff1d9dc5df4bc3bd0d481c19b45463.mailgun.org"
 
   def send_email(email, subject, message) do
     send_email to: email,
